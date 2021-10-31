1cc0a07c96b0fe0da17b1e24ef31a5b26b44207f
maximkuznecovv <maximkuznecovv@gmail.com>
2021-10-31 22:16:52 +0200

    HW5 (work!!!)

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,16 +1,17 @@
 import React from "react";
-import s from './Header.module.css'
+import s from "./Header.module.css"
 import {NavLink} from "react-router-dom";
 import {PATH} from "./Routes";
 
 function Header() {
     return (
         <div className={s.header}>
-            // add NavLink
+            {/*// add NavLink*/}
             <NavLink to={PATH.PRE_JUNIOR} className={s.link} activeClassName={s.active}>pre-junior</NavLink>
             <NavLink to={PATH.JUNIOR} className={s.link} activeClassName={s.active}>junior</NavLink>
             <NavLink to={PATH.JUNIOR_PLUS} className={s.link} activeClassName={s.active}>junior+</NavLink>
             <div className={s.block}/>
+            {/*<div className={s.block}>==</div>*/}
         </div>
     );
 }
